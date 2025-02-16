5ae1f3fb92b9366130c32dfdf77847a42653485c
Jigokuneenja <140099538+Jigokuneenja@users.noreply.github.com>
2025-02-15 20:32:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,39 +4,39 @@ let about = document.getElementById("about")
 let projects = document.getElementById("projects")
 let contactMe = document.getElementById("contactMe")
 function showHome() {
-  if (currenPage != "home") {
+  if (currentPage != "home") {
     home.style.display = "block"
-    about.style.display = "none"
-    projects.style.display = "none"
-    contactMe.style.display = "none"
+    //about.style.display = "none"
+    //projects.style.display = "none"
+    //contactMe.style.display = "none"
     currentPage = "home"
   }
 }
 function showAbout() {
-  if (currenPage != "about") {
+  if (currentPage != "about") {
     home.style.display = "none"
-    about.style.display = "block"
-    projects.style.display = "none"
-    contactMe.style.display = "none"
+    //about.style.display = "block"
+    //projects.style.display = "none"
+    //contactMe.style.display = "none"
     currentPage = "about"
-    print(home)
+    console.log(currentPage);
   }
 }
 function showProjects() {
-  if (currenPage != "projects") {
+  if (currentPage != "projects") {
     home.style.display = "none"
-    about.style.display = "none"
-    projects.style.display = "block"
-    contactMe.style.display = "none"
+    //about.style.display = "none"
+    //projects.style.display = "block"
+    //contactMe.style.display = "none"
     currentPage = "projects"
   }
 }
 function showContactMe() {
-  if (currenPage != "ContactMe") {
+  if (currentPage != "contactMe") {
     home.style.display = "none"
-    about.style.display = "none"
-    projects.style.display = "none"
-    contactMe.style.display = "block"
-    currentPage = "ContactMe"
+    //about.style.display = "none"
+    //projects.style.display = "none"
+    //contactMe.style.display = "block"
+    currentPage = "contactMe"
   }
 }
